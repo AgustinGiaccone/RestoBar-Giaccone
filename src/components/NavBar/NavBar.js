167b0908b157b95d40d278839a13f94992332c34
Agu <agustintgiaccone1999@gmail.com>
2022-08-21 11:01:23 -0300

    sube carpeta

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import logo from '../../logo.svg';
+import './NavBar.css'
+
+const NavBar = () =>{
+    const style = {}
+    return(
+        <div className='navbar-conteiner'>
+            <div>
+                <img width={'200px'} src={logo} alt="logo"/>
+            </div>
+            <div>
+                <ul>
+                    <li><a href='#'>Inicio</a></li>
+                    <li><a href='#'>Nosotros</a></li>
+                    <li><a href='#'>Contacto</a></li>
+                </ul>
+            </div>
+            <div>
+                <button className='button-login'>Login</button>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
