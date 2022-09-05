97de14c19d8f3cff693daa6cf5eb72f5d667f1cd
Agu <agustintgiaccone1999@gmail.com>
2022-09-05 20:35:12 -0300

    crea tarjetas sin estilo

--- src/components/itemlistconteiners/ItemListContainer.jsx
+++ src/components/itemlistconteiners/ItemListContainer.jsx
@@ -1,5 +1,8 @@
-import './ItemListContainer.css'
-import ItemCount from '../ItemCount/ItemCount.js'
+import './ItemListContainer.css';
+import ItemCount from '../ItemCount/ItemCount.js';
+import data from './mock-data';
+import { useState, useEffect } from "react";
+import ItemList from '../ItemList/ItemList.js'
 
 const onAdd = (cantidadComprada) =>{
     console.log(`compraste ${cantidadComprada} unidades`)
@@ -7,11 +10,31 @@ const onAdd = (cantidadComprada) =>{
 
 const ItemListConteiners  = ({titulo, miNombre, subtitulo}) =>{
     console.log(subtitulo)
+    const [items, setItems] = useState([]);
+
+    const getData = new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve(data)
+        }, 2000);
+    })
+        useEffect(() =>{
+            getData.then((result)=>{
+                setItems(result);
+                console.log(result);
+            })
+        }, []);
     return(
         <div className='conteiner'>
             <h1>{titulo}</h1>
             <p>{miNombre}</p>
             <ItemCount inicial ={1} stock={10} onAdd={onAdd}/>
+            {
+                items.length > 0 ?(
+            <ItemList Listado = {items}/>
+                ) :(
+                    <div>Cargando....</div>
+                )
+            }
         </div>
     )
 }
---
+++ src/components/itemlistconteiners/mock-data.js
@@ -0,0 +1,32 @@
+import HamburguesaImg from '../img/hamburguesa.jpg';
+import GaseosaImg from '../img/gaseosa.jpg';
+import PapasImg from '../img/papas.jpg';
+
+const data = [
+    {
+        id: 1,
+        nombre:'hamburguesa',
+        descripcion:'medallon de carne, chedar, tomate y lechuga',
+        precio:'$300',
+        imagen: HamburguesaImg,
+        cantidad:10
+    },
+    {
+        id: 2,
+        nombre:'gaseosa',
+        descripcion:'Coco Cola',
+        precio:'$200',
+        imagen: GaseosaImg,
+        cantidad:10
+    },
+    {
+        id: 3,
+        nombre:'papas fritas',
+        descripcion:'crujientes y saladas',
+        precio:'$100',
+        imagen: PapasImg,
+        cantidad:10
+    },
+];
+
+export default data;
